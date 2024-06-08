479b2f629563630e502077d3360e597a24097303
Mostafa Mahmoud Bahgat <79370687+MostafaMahmoudBahgat98@users.noreply.github.com>
2024-06-09 00:08:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+tasks = []
+
 @app.route('/')
-def hello():
-    return "Hello, DevOps!"
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form['task']
+    tasks.append(task)
+    return index()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(debug=True)
